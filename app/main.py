be983b179997a7763bbe08cb840d4606dcfd3df6
Tara Udani <taraudani@Taras-MacBook-Air.local>
2024-11-18 14:48:48 -0500

    updated main.py

--- app/main.py
+++ app/main.py
@@ -47,8 +47,6 @@ def square(e: int):
     
 @app.get('/genres')
 def get_genres():
-    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
-    cur=db.cursor()
     query = "SELECT * FROM genres ORDER BY genreid;"
     try:    
         cur.execute(query)
@@ -57,16 +55,15 @@ def get_genres():
         json_data=[]
         for result in results:
             json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
         return(json_data)
     except Error as e:
         return {"Error": "MySQL Error: " + str(e)}
-    cur.close
-    db.close
+    
 
 @app.get('/songs')
 def get_songs():
-    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
-    cur=db.cursor()
     query = "SELECT songs.title, songs.album, songs.artist, songs.year, genres.genre FROM `songs` JOIN genres ON genres.genreid = songs.genre;;"
     try:    
         cur.execute(query)
@@ -75,9 +72,10 @@ def get_songs():
         json_data=[]
         for result in results:
             json_data.append(dict(zip(headers,result)))
+        cur.close()
+        db.close()
         return(json_data)
     except Error as e:
         return {"Error": "MySQL Error: " + str(e)}
-    cur.close
-    db.close
+ 
         
---
+++ pacman/app.py
@@ -0,0 +1,71 @@
+import os
+import boto3
+import json
+import mysql.connector
+from chalice import Chalice
+
+app = Chalice(app_name='pacman')
+app.debug = True
+
+
+# Set the value of APP_BUCKET_NAME in the .chalice/config.json file.
+S3_BUCKET = 'hav7tz-dp1-spotify'
+s3 = boto3.client('s3')
+
+baseurl = 'http://hav7tz-dp1-spotify.s3-website-us-east-1.amazonaws.com/'
+
+DBHOST = os.getenv('DBHOST')
+DBUSER = os.getenv('DBUSER')
+DBPASS = os.getenv('DBPASS')
+DB = os.getenv('DB')
+db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+cur = db.cursor()
+
+_SUPPORTED_EXTENSIONS = (
+    '.json'
+)
+
+@app.on_s3_event(bucket=S3_BUCKET, events=['s3:ObjectCreated:*'])
+def s3_handler(event):
+  if _is_json(event.key):
+    # get the file, read it, load it into JSON as an object
+    response = s3.get_object(Bucket=S3_BUCKET, Key=event.key)
+    text = response["Body"].read().decode()
+    data = json.loads(text)
+
+    # parse the data fields 1-by-1 from 'data'
+    TITLE = data['title']
+    ALBUM = data['album']
+    ARTIST = data['artist']
+    YEAR = data['year']
+    GENRE = data['genre']
+
+    # get the unique ID for the bundle to build the mp3 and jpg urls
+    # you get 5 data points in each new JSON file that arrives, but
+    # you need 7 fields for the INSERT. The two additional values are
+    # URLs you must formulate given you know the unique ID these files
+    # are named.
+    keyhead = event.key
+    identifier = keyhead.split('.')
+    ID = identifier[0]
+    MP3 = baseurl + ID + '.mp3'
+    IMG = baseurl + ID + '.jpg'
+
+    app.log.debug("Received new song: %s, key: %s", event.bucket, event.key)
+
+    # try to insert the song into the database
+    try:
+      add_song = ("INSERT INTO songs "
+               "(title, album, artist, year, file, image, genre) "
+               "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+      song_vals = (TITLE, ALBUM, ARTIST, YEAR, MP3, IMG, GENRE)
+      cur.execute(add_song, song_vals)
+      db.commit()
+
+    except mysql.connector.Error as err:
+      app.log.error("Failed to insert song: %s", err)
+      db.rollback()
+
+# perform a suffix match against supported extensions
+def _is_json(key):
+  return key.endswith(_SUPPORTED_EXTENSIONS)
